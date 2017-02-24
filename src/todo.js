2d1371c6396cbf7d3cbe83edc81206e9c6e287cf
Troy Kearney <tkearney18@gmail.com>
2017-02-24 11:56:22 -0500

    Updated the todo.js and added it back to the project

--- src/todo.js
+++ src/todo.js
@@ -31,11 +31,14 @@ var TodoBanner = React.createClass({
 		);
 	}
 });
-
+/**
+ * TodoList renders out the TodoListItems
+ * When called it returns a ul which uses the createItem function to call the TodoListItem for 
+ */
 var TodoList = React.createClass({
 	render: function() {
         // in a list that uses the .map function, keys are needed
-        // Math.random() is used to generate a key.
+        // Math.random() is used to generate a key. this is also an example of a closure.
         var id = Math.random();
 		var createItem = function(itemText, id) {
 			return (
@@ -46,36 +49,51 @@ var TodoList = React.createClass({
 	}
 });
 
+/**
+ * TodoListItem renders out the input provided from the form. Only has access to this.props.
+ */
 var TodoListItem = React.createClass({
 	render: function(){
 		return (
-			<li>{this.state.item} - {this.state.desc}</li>
+			<li>{this.props.children.item} - {this.props.children.desc} </li>
 		);
 	}
 });
-
+ /**
+  * TodoForm render the form, provides getInitialState, onChange
+  */
 var TodoForm = React.createClass({
+	// getInitialState to set the default values
 	getInitialState: function() {
 		return {item: '', desc: ''};
 	},
+	//handleSubmit adds the properties on onFormSubmit and sets the state
 	handleSubmit: function(e){
 		e.preventDefault();
-		this.props.onFormSubmit(this.state.item, this.state.desc);
-		this.setState({item: '', desc: ''});
-		ReactDOM.findDOMNode(this.refs.item).focus();
+		this.props.onFormSubmit({item: this.state.item, desc: this.state.desc});
+		this.setState({item: this.state.item, desc: this.state.desc});
+		//ReactDOM.findDOMNode(this.refs.item).focus();
+		this.getInitialState();
 		return;
 	},
+	// onChange handler to change the state. only one is needed when using the form of e.target.name
 	onChange: function(e){
 		this.setState({
 			[e.target.name]: e.target.value
 		});
 	},
-    
+    //rendering the actual form. using the name attribute allows us to use the single above
 	render: function(){
 		return (
 			<form onSubmit={this.handleSubmit}>
-				<input type='text' name='item' ref='item' onChange={this.onChange} value={this.state.item}/>
-                <input type='text' name='desc' ref='desc' onChange={this.onChange} value={this.state.desc}/>
+				<div className="row">
+					<label htmlFor="item">Todo item: </label>
+					<input type='text' name='item' ref='item' id="item" onChange={this.onChange} value={this.state.item}/>
+				</div>
+				<div className="row">
+                	<label htmlFor="desc">Description: </label>
+					<input type='text' name='desc' ref='desc' onChange={this.onChange} value={this.state.desc}/>
+				</div>
 				<input type='submit' value='Add'/>
 			</form>
 		);
